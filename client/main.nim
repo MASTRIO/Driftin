0aebc51919f0f43bb112e08eb53b66660b6b9b17
MASTRIO <mastrio5772@gmail.com>
2022-03-29 16:59:50 +1000

    progress on packet system

--- client/main.nim
+++ client/main.nim
@@ -1,4 +1,13 @@
-import netty
+import fidget, fidget/opengl/base, netty, vmath, bumpy, std/json
+
+type
+  Player = object
+    position, velocity: Vec2
+  
+  Packet = object
+    id: int
+    packet_type: string
+    packet_data: # TODO: make this work
 
 # create client and connect to the server
 var
@@ -8,14 +17,53 @@ var
   client = newReactor()
   c2s = client.connect(ip, port)
 
-#client.send(c2s, "hi")
+let client_id = 1
+let this_player: Player = Player(
+  position: Vec2(),
+  velocity: Vec2()
+)
 
-# Client loop
-while true:
+#[
+proc create_packet(id: int, packet_type: string, packet_data: array[10, string]): string =
+  var json = %*
+    [
+      {
+        "id": id,
+        "packet_type": packet_type,
+        "packet_data": packet_data
+      }
+    ]
+  return $json
+]#
+
+client.send(c2s, Packet(id: client_id, packet_type: "player_data", packet_data: [this_player.position]).toFlatty())
+
+# Used to draw stuff on screen
+# Runs at monitor refresh rate (48-60 hz)
+proc drawMain() =
+  frame "main":
+    box 0, 0, 620, 140
+    for i in 0 .. 4:
+      group "block":
+        box 20+i*120, 20, 100, 100
+        fill "#2B9FEA"
+
+# Client loop code
+# runs every game tick (around 240 hz)
+proc tickMain() =
   # Update client
   client.tick()
 
   # Loop though server messages
   for msg in client.messages:
-    # print message data
-    echo "GOT MESSAGE: ", msg.data
+    echo msg.data
+
+startFidget(
+  draw = drawMain,
+  tick = tickMain,
+  w = 400,
+  h = 400,
+  openglVersion = (4, 1),
+  msaa = msaa4x,
+  mainLoopMode = RepaintSplitUpdate
+)
--- server/main.nim
+++ server/main.nim
@@ -1,4 +1,4 @@
-import netty
+import netty, fidget, std/json
 
 # Create server
 var
@@ -7,14 +7,21 @@ var
 
   server = newReactor(ip, port)
 
+echo "Server online at ", ip, ":", port
+
 # Server update loop
 while true:
   # Update server
   server.tick()
 
+  for connection in server.newConnections:
+    echo "Player ip ", connection.address, " has connected"
+  for connection in server.deadConnections:
+    echo "Player ip ", connection.address, " has disconnected"
+
   # Loop though any messages the server has received from clients
   for msg in server.messages:
-    # print message data
-    echo "GOT MESSAGE: ", msg.data
-    # echo message back to the client
-    server.send(msg.conn, "you said:" & msg.data)
+    # Yeet that data straight to every connected client
+    # TODO: replace this with a much less hackable system
+    for connection in server.connections:
+      server.send(connection, msg.data)
