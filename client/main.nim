c645fea33ced559090920ccf0816816e89eadd13
MASTRIO <mastrio5772@gmail.com>
2022-04-03 00:33:24 +1000

    OH MY GOD IT FINALLY WORKS LETS GOOOOOOOOOOOOOOOOO

--- client/main.nim
+++ client/main.nim
@@ -1,4 +1,4 @@
-import fidget, fidget/opengl/base, netty, flatty, vmath, bumpy, std/json, std/lists, std/strutils, std/strformat
+import boxy, opengl, windy, netty, flatty, vmath, std/strutils, std/strformat
 
 type
   Player = object
@@ -9,10 +9,6 @@ type
     packet_type: string
     packet_data: string
 
-  NetworkedPlayerData = object
-    id: int
-    position: Vec2
-
 # create client and connect to the server
 var
   ip = "127.0.0.1"
@@ -22,39 +18,29 @@ var
   c2s = client.connect(ip, port)
 
 let client_id = 1
-let this_player: Player = Player(
+var this_player: Player = Player(
   position: Vec2(),
   velocity: Vec2()
 )
+this_player.position.x = 100
+this_player.position.y = 100
 
-var network_players = initDoublyLinkedList[NetworkedPlayerData]()
+let window_size = ivec2(600, 300)
+let window = newWindow("nim client test", window_size)
+makeContextCurrent(window)
 
-#[
-proc create_packet(id: int, packet_type: string, packet_data: array[10, string]): string =
-  var json = %*
-    [
-      {
-        "id": id,
-        "packet_type": packet_type,
-        "packet_data": packet_data
-      }
-    ]
-  return $json
-]#
+loadExtensions()
 
-# Used to draw stuff on screen
-# Runs at monitor refresh rate (48-60 hz)
-proc drawMain() =
-  frame "main":
-    box 0, 0, 620, 140
-    #for i in 0 .. 4:
-    #  group "block":
-    #    box 20+i*120, 20, 100, 100
-    #    fill "#2B9FEA"
+let bxy = newBoxy()
 
-# Client loop code
-# runs every game tick (around 240 hz)
-proc tickMain() =
+bxy.addImage("player", readImage("assets/sprites/why_he_green_tho.png"))
+
+var frame: int
+
+proc displayLoop() =
+  # Start frame
+  bxy.beginFrame(window_size)
+
   # Update client
   client.tick()
 
@@ -64,34 +50,20 @@ proc tickMain() =
   # Loop though server messages
   for msg in client.messages:
     var message_data: Packet = msg.data.fromFlatty(Packet)
-    
+
     if message_data.packet_type == "player_position":
-      var player_number = 0
-      for networked_player in network_players:
-        if networked_player.id == message_data.id:
-          var setup_pos = Vec2()
-          let coords = message_data.packet_data.split(",")
-          setup_pos.x = parseFloat(coords[0])
-          setup_pos.y = parseFloat(coords[1])
-          network_players[player_number].position = setup_pos
-        player_number += 1
+      var network_pos = Vec2()
+      var split_data = message_data.packet_data.split(",")
+      network_pos.x = parseFloat(split_data[0])
+      network_pos.y = parseFloat(split_data[1])
 
+      bxy.drawImage("player", network_pos, angle = frame.float / 100)
 
-    #[
-    if message_data.packet_type == "player_position":
-      var coords = message_data.packet_data.split(",")
-      frame "main":
-        group "block":
-          box parseFloat(coords[0]), parseFloat(coords[1]), 100, 100
-          fill "#2B9FEA"
-    ]#
+  # End frame
+  bxy.endFrame()
+  window.swapBuffers()
+  inc frame
 
-startFidget(
-  draw = drawMain,
-  tick = tickMain,
-  w = 400,
-  h = 400,
-  openglVersion = (4, 1),
-  msaa = msaa4x,
-  mainLoopMode = RepaintSplitUpdate
-)
+while not window.closeRequested:
+  displayLoop()
+  pollEvents()
