e6f1b64f62ac120c6c65e3b177194a8ba8a126bb
MASTRIO <mastrio5772@gmail.com>
2022-04-03 01:44:16 +1000

    how tf do you get keyboard input in nim

--- client/main.nim
+++ client/main.nim
@@ -1,13 +1,8 @@
-import boxy, opengl, windy, netty, flatty, vmath, std/strutils, std/strformat
+import boxy, opengl, windy, netty, vmath, std/strutils, std/strformat
 
 type
   Player = object
     position, velocity: Vec2
-  
-  Packet = object
-    id: int
-    packet_type: string
-    packet_data: string
 
 # create client and connect to the server
 var
@@ -37,6 +32,9 @@ bxy.addImage("player", readImage("assets/sprites/why_he_green_tho.png"))
 
 var frame: int
 
+proc create_packet(id: int, packet_type: string, packet_data: string): string =
+  return intToStr(id) & "||" & packet_type & "||" & packet_data
+
 proc displayLoop() =
   # Start frame
   bxy.beginFrame(window_size)
@@ -45,19 +43,22 @@ proc displayLoop() =
   client.tick()
 
   # Update player position on all clients
-  client.send(c2s, Packet(id: client_id, packet_type: "player_position", packet_data: fmt"{this_player.position.x},{this_player.position.y}").toFlatty())
+  client.send(c2s, create_packet(client_id, "player_position", fmt"{this_player.position.x},{this_player.position.y}"))
 
   # Loop though server messages
   for msg in client.messages:
-    var message_data: Packet = msg.data.fromFlatty(Packet)
+    var message_data = msg.data.split("||")
 
-    if message_data.packet_type == "player_position":
+    if message_data[1] == "player_position":
       var network_pos = Vec2()
-      var split_data = message_data.packet_data.split(",")
+      var split_data = message_data[2].split(",")
       network_pos.x = parseFloat(split_data[0])
       network_pos.y = parseFloat(split_data[1])
 
       bxy.drawImage("player", network_pos, angle = frame.float / 100)
+
+  # Player movement
+  this_player.position.y += 0.1
 
   # End frame
   bxy.endFrame()
